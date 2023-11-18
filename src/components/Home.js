5274407b5bcb139c254243d5c4bc7ba2608565a6
KevinMKW <mkwkvn@gmail.com>
2023-11-18 15:13:39 +0300

    Test TMDB API in the app.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Home = () => {
+
+    const getMovies = () => {
+        try{
+            fetch("https://api.themoviedb.org/3/discover/movie?api_key=769ee3e8678ca9401b066ed3ef16424a")
+            .then(res => res.json())
+            .then(json => console.log(json.results))
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(()=>{getMovies()},[])
+
     return (
         <div>
             Home

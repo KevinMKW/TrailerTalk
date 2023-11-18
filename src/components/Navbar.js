4f8dc990de4969b204c8d7711d88b6d83167e514
KevinMKW <mkwkvn@gmail.com>
2023-11-18 16:17:42 +0300

    Add header image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Navbar = () => {
+
+    const [movies,setMovies] = useState([]);
+
+    const getMovies = () => {
+        try{
+            fetch("https://api.themoviedb.org/3/discover/movie?api_key=769ee3e8678ca9401b066ed3ef16424a")
+            .then(res => res.json())
+            .then(json => setMovies(json.results))
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(()=>{getMovies()},[])
+
+    console.log(movies[0])
+
     return (
-        <div>
-            Navbar
+        <div style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${movies[1]?.poster_path})`,backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundSize:"cover",height:"400px",width:"100%"}}>
         </div>
     );
 }

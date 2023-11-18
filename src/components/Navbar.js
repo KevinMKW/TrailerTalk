f83ee4d44eb11a3b39b2126017002432924fc861
KevinMKW <mkwkvn@gmail.com>
2023-11-18 18:19:57 +0300

    Add modifications on the header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import netflix from '../images/netflix_logo.png'
+import { Button } from '@mui/material';
 
 const Navbar = () => {
 
@@ -16,10 +18,17 @@ const Navbar = () => {
 
     useEffect(()=>{getMovies()},[])
 
-    console.log(movies[0])
-
     return (
-        <div style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${movies[1]?.poster_path})`,backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundSize:"cover",height:"400px",width:"100%"}}>
+        <div style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${movies[1]?.poster_path})`,backgroundPosition:"center",backgroundRepeat:"no-repeat",backgroundSize:"cover",minHeight:"500px",width:"100%"}}>
+            <div style={{display:'flex',justifyContent:'space-between',padding:'20px'}}>
+                <img alt='Netflix logo' style={{width:"90px",height:"70px"}} src={netflix}/>
+                <Button color='error' variant='contained' sx={{height:'40px'}}>SignIn</Button>
+            </div>
+            <div style={{padding:'20px'}}>
+                <h1 style={{color:"#f1f1f1", fontSize:'70px',fontFamily:'initial'}}>{movies[1]?.original_title}</h1>
+                <h4 style={{color:'#f1f1f1'}}>{movies[1]?.overview}</h4>
+            </div>
+            <Button variant='contained' sx={{color:'black',bgcolor:'white'}}>View Trailer</Button>
         </div>
     );
 }

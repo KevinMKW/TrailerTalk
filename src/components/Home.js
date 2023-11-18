fc35ade35f35fb14ff2a10181e64595338d934b0
KevinMKW <mkwkvn@gmail.com>
2023-11-18 16:01:44 +0300

    Add home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import { Box, Card, CardContent, CardMedia, Grid } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+
+    const [movies,setMovies] = useState([]);
 
     const getMovies = () => {
         try{
             fetch("https://api.themoviedb.org/3/discover/movie?api_key=769ee3e8678ca9401b066ed3ef16424a")
             .then(res => res.json())
-            .then(json => console.log(json.results))
+            .then(json => setMovies(json.results))
         } catch(err) {
             console.log(err)
         }
@@ -14,9 +17,29 @@ const Home = () => {
 
     useEffect(()=>{getMovies()},[])
 
+    console.log(movies)
+
     return (
         <div>
-            Home
+            <Grid container spacing={2}>
+                {movies.map((movie) => {
+                return <Grid item xs={3}>
+                    <Box>
+                        <Card>
+                            <CardMedia 
+                            component="img"
+                            height="140"
+                            image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}>
+
+                            </CardMedia>
+                            <CardContent>
+                            </CardContent>
+                        </Card>
+                    </Box>
+                </Grid>
+            })}
+            </Grid>
+            
         </div>
     );
 }

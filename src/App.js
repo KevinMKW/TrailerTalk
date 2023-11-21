cc74a43156a827b6d95f152ea9ca0acc44788052
KevinMKW <mkwkvn@gmail.com>
2023-11-21 14:39:43 +0300

    add navbar and home in the main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "./components/Main";
+import Signin from "./components/Signin";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="/signin" element={<Signin/>} />
+      </Routes>
+      
     </div>
   );
 }
